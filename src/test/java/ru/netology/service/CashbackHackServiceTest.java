e27ef4133ec08917fe6b90d9ab9cc7da38c45815
Telepuzique <g6ko8a@gmail.com>
2023-12-17 03:18:17 +0300

    Add TestNG tests

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,40 @@
+package ru.netology.service;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldCalculateCashbackWhenAmountIs0() {
+
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 1000;
+        int actual = service.remain(0);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateCashback() {
+
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 236;
+        int actual = service.remain(1764);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateCashbackWhenAmountIs1000() {
+
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 0;
+        int actual = service.remain(1000);
+
+        Assert.assertEquals(expected, actual);
+    }
+}
